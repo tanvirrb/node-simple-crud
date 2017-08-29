5acb180c5251f2100075bcdd5bdd46b54ce31fdb
tanvirrb <tanvir.rb@outlook.com>
2017-08-29 22:25:54 +0600

    test added for company get by id

--- test/company.js
+++ test/company.js
@@ -8,15 +8,19 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 
-describe('Empty Company Collection before testing started', function() {
-    it('should empty company collection', function(done) {
-        Company.collection.drop();
-        done();
+describe('Empty Company Collection before test started', function() {
+
+    it('should empty Company Collection before test started', function() {
+        beforeEach(function(done){
+            Company.collection.drop();
+            done();
+        });
     });
 });
 
 describe('Test Company RESTful API', function() {
     this.timeout(10000);
+
     it('should list all companies on /api/companies GET', function(done) {
         chai.request(server)
             .get('/api/companies')
@@ -28,7 +32,32 @@ describe('Test Company RESTful API', function() {
             });
     });
 
-    it('should create a company on /api/companies POST', function(done) {
+    it('should get a company with ID on /api/companies/<id> get', function(done) {
+        var newCompany = new Company({
+            name: 'Google Inc',
+            type: 'Search Engine'
+        });
+        newCompany.save(function (err, data) {
+            chai.request(server)
+                .get('/api/companies/'+data._id)
+                .end(function(err, res){
+                    //console.log(res);
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('_id');
+                    res.body.should.have.property('name');
+                    res.body.name.should.equal('Google Inc');
+                    res.body.should.have.property('type');
+                    res.body.type.should.equal('Search Engine');
+                    res.body.should.not.have.property('logo');
+                    done();
+                });
+        })
+
+    });
+
+    it('should create a company WITHOUT LOGO FILE on /api/companies POST', function(done) {
         chai.request(server)
             .post('/api/companies')
             .field('name', 'Facebook')
@@ -46,5 +75,15 @@ describe('Test Company RESTful API', function() {
                 res.body.should.not.have.property('logo');
                 done();
             });
+    });
+});
+
+describe('Empty Company Collection after test finished', function() {
+
+    it('should empty Company Collection after test finished', function() {
+        afterEach(function(done){
+            Company.collection.drop();
+            done();
+        });
     });
 });
