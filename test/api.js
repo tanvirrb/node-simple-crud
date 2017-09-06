6896b07d06bbb049421ba8480957aa3056b37a29
tanvirrb <tanvir.rb@outlook.com>
2017-09-06 13:13:55 +0600

    company test put done partially

--- test/api.js
+++ test/api.js
@@ -8,17 +8,23 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 
-describe('Empty Company Collection before test started', function() {
+describe('Empty Company Collection before every test started', function() {
 
     it('should empty Company Collection before test started', function() {
         beforeEach(function(done){
-            Company.collection.drop();
-            done();
+            let newCompany = new Company({
+                name:'Zapier',
+                type: 'Web Service'
+            });
+
+            newCompany.save(function (err) {
+                done();
+            })
         });
     });
 });
 
-describe('Empty Company Collection after test finished', function() {
+describe('Empty Company Collection after every test finished', function() {
 
     it('should empty Company Collection after test finished', function() {
         afterEach(function(done){
@@ -31,26 +37,31 @@ describe('Empty Company Collection after test finished', function() {
 describe('Test Company RESTful API', function() {
     this.timeout(10000);
 
-    it('should list all companies on /api/companies GET', function(done) {
+    it('should list all companies on /api/companies GET', function (done) {
         chai.request(server)
             .get('/api/companies')
-            .end(function(err, res){
+            .end(function (err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('array');
+                res.body[0].should.have.property('_id');
+                res.body[0].should.have.property('name');
+                res.body[0].should.have.property('type');
+                res.body[0].name.should.equal('Zapier');
+                res.body[0].type.should.equal('Web Service');
                 done();
             });
     });
 
-    it('should get a company with ID on /api/companies/<id> get', function(done) {
+    it('should get a company with ID on /api/companies/:id get', function (done) {
         let newCompany = new Company({
             name: 'Google Inc',
             type: 'Search Engine'
         });
         newCompany.save(function (err, data) {
             chai.request(server)
-                .get('/api/companies/'+data._id)
-                .end(function(err, res){
+                .get('/api/companies/' + data._id)
+                .end(function (err, res) {
                     //console.log(res);
                     res.should.have.status(200);
                     res.should.be.json;
@@ -67,12 +78,12 @@ describe('Test Company RESTful API', function() {
 
     });
 
-    it('should create a company WITHOUT LOGO FILE on /api/companies POST', function(done) {
+    it('should create a company WITHOUT LOGO FILE on /api/companies POST', function (done) {
         chai.request(server)
             .post('/api/companies')
             .field('name', 'Yahoo')
             .field('type', 'News')
-            .end(function(err, res){
+            .end(function (err, res) {
                 //console.log(res);
                 res.should.have.status(200);
                 res.should.be.json;
@@ -84,6 +95,28 @@ describe('Test Company RESTful API', function() {
                 res.body.type.should.equal('News');
                 res.body.should.not.have.property('logo');
                 done();
+            });
+    });
+
+    it('should update a single company on /api/companies/:id PUT', function (done) {
+        chai.request(server)
+            .get('/api/companies')
+            .end(function (err, res) {
+                chai.request(server)
+                    .put('/api/companies/' + res.body[0]._id)
+                    .field('name', 'Zappi')
+                    .end(function (err, res) {
+                        res.should.have.status(200);
+                        res.should.be.json;
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('ok');
+                        res.body.ok.should.equal(1);
+                        res.body.should.have.property('nModified');
+                        res.body.nModified.should.equal(1);
+                        res.body.should.have.property('n');
+                        res.body.n.should.equal(1);
+                        done();
+                    });
             });
     });
 });
